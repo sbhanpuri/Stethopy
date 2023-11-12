19d51c27a47e29050768574d931c174db54f3859
anilachundi <achundi2@illinois.edu>
2023-11-12 16:01:06 -0600

    adding profile page to the app

--- Frontend/stethopy_app/pages/HeartRateScreen.js
+++ Frontend/stethopy_app/pages/HeartRateScreen.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet} from 'react-native';
 
 function HeartRateScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Your Heart Rate Screen</Text>
+    <View style={styles.container}>
+      <Text style={{ fontSize: 20, fontFamily: 'HelveticaNeue-Thin', fontWeight: 'bold', color: 'black' }}>
+        WAVE FILE
+      </Text>
+      <Button
+        mode="contained"
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+        style={styles.button}
+      />
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'hsla(200, 100%, 77%, 1)', // Change the background color as needed
+  },
+  title: {
+    fontSize: 24, // Change the font size as needed
+    marginBottom: 20, // Adjust spacing between title and button
+  },
+  button: {
+    width: '90%', // Adjust the width as needed
+    marginTop: 20, // Adjust the margin as needed
+  },
+});
+
 
 export default HeartRateScreen;
--- Frontend/stethopy_app/pages/Welcome.js
+++ Frontend/stethopy_app/pages/Welcome.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet} from 'react-native';
 
 const WelcomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Welcome to the App!</Text>
+    <View style={styles.container}>
+      <Text style={{ fontSize: 50, fontFamily: 'HelveticaNeue-Thin', fontWeight: 'bold', color: 'black' }}>
+        STETHOPY
+      </Text>
+      
       <Button
+        mode="contained"
         title="Go to Heart Rate Screen"
         onPress={() => navigation.navigate('HeartRateScreen')}
+        style={styles.button}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'hsla(199, 100%, 75%, 1)', // Change the background color as needed
+  },
+  title: {
+    fontSize: 24, // Change the font size as needed
+    marginBottom: 20, // Adjust spacing between title and button
+  },
+  button: {
+    width: '90%', // Adjust the width as needed
+    marginTop: 20, // Adjust the margin as needed
+  },
+});
 
 export default WelcomeScreen;
