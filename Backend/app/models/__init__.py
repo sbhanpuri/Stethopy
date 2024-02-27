29c4ecfcd00f731642be64f0f3b561229459ec8d
“jonathancchsu” <jhsucoding@gmail.com>
2024-02-27 16:52:27 -0600

    restructing backend

---
+++ Backend/app/models/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from config import Config
+
+def create_app():
+  app = Flask(__name__)
+  app.config.from_object(Config)
+  
+  from .models import db
+  db.init_app(app)
+  
+  from .routes import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+  
+  return app
