90614f24d6327244d3cf4a8241e3f348b2c5144b
PVAnd124 <pv16@illinois.edu>
2024-03-03 11:21:16 -0600

    Added instructional graphic to HeartRecordStep1.js page

--- Frontend/stethopy_app/src/pages/HeartRateScreenPages/NewSessionPages/HeartRecordStep1.js
+++ Frontend/stethopy_app/src/pages/HeartRateScreenPages/NewSessionPages/HeartRecordStep1.js
@@ -1,48 +1,76 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import BottomTabNavigator from '../../../components/BottomTabNavigator';
+import RecordPageImage from '../../../components/images/RecordPage1Image.jpg'
+import Circle from '../../../components/images/Red_circle.svg.png'
+//import { fetchData } from './ListeningSessions/RecordButtonPress';
+//import { FontAwesome } from '@expo/vector-icons';
+//import ListeningSessions from './ListeningSessions';
 
 const HeartRecordStep1 = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 30, fontFamily: 'HelveticaNeue-Thin', fontWeight: 'bold', color: 'black' }}>
-        HEART RECORD STEP 1
-      </Text>
-
-
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('HeartRecordStep2');
-          console.log('You tapped the button!');
-        }}
-        style={styles.customButton}
-      >
-        <Text style={styles.buttonLabel}>Heart Record Step 2</Text>
-      </TouchableOpacity>
-      
-      <BottomTabNavigator></BottomTabNavigator>
-    </View>
-
-
+      <Text style={styles.title}>
+          Measurement 1: Right heart
+        </Text>
+        <Image 
+          source={RecordPageImage}
+          style={styles.image}
+        />
+        <Image
+          source={Circle}
+          style={styles.circle}
+        />
+        
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('HeartRecordStep2');
+            console.log('You tapped the button!');
+          }}
+          style={styles.customButton}
+        >
+          <Text style={styles.buttonText}>Heart Record Step 2</Text>
+        </TouchableOpacity>
+        
+        
+        <BottomTabNavigator></BottomTabNavigator>
+      </View>
   );
 };
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: 'hsla(199, 100%, 75%, 1)', // Change the background color as needed
+    backgroundColor: 'hsla(215, 100%, 60%, 1)', // Change the background color as needed
   },
   title: {
     fontSize: 24, // Change the font size as needed
     marginBottom: 20, // Adjust spacing between title and button
+    fontFamily: 'HelveticaNeue-Thin',
+    fontWeight: 'bold',
+    color: 'white',
+    alignSelf: 'center',
+    marginTop: 100,
+    marginBottom: -100,
   },
   customButton: {
     backgroundColor: 'white', // Change button background color
     padding: 15,
-    borderRadius: 10, // Adjust border radius for rounded corners
-    marginTop: 20,
+    borderRadius: 50, // Adjust border radius for rounded corners
+    marginTop: 250,
+    marginBottom: 250,
+    width: 200,
+    height: 50,
+  },
+  recordButton: {
+    backgroundColor: 'red',
+    borderRadius: 50,
+    marginTop: 160,
+    marginBottom: -175,
     width: 200,
     height: 50,
   },
@@ -50,6 +78,27 @@ const styles = StyleSheet.create({
     width: '90%', // Adjust the width as needed
     marginTop: 20, // Adjust the margin as needed
   },
+  buttonText: {
+    color: 'black', // Change text color
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 500,
+    height: 200,
+    marginTop: 175,
+    marginBottom: -150,
+    resizeMode: 'contain', // Adjust the resizeMode as per your requirement
+  },
+  circle: {
+    position: 'relative',
+    left: -22,
+    padding: 15, //change the diameter of the red circle
+    width: 30,
+    height: 30,
+    marginTop: 7,
+  }
 });
 
 export default HeartRecordStep1;
